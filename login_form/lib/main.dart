168e1ce052cb5dcc142ad7698a2485eb2c1405f9
descalante16 <escalante.desirie1@gmail.com>
2023-11-28 23:35:35 +0800

    initial commit

---
+++ login_form/lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MainPage());
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: FirstPage());
+  }
+}
+
+class FirstPage extends StatefulWidget {
+  const FirstPage({super.key});
+
+  @override
+  State<FirstPage> createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.yellow[400],
+        title: Text(
+          'Log in Form',
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        leading: Icon(
+          Icons.code,
+          color: Colors.black,
+        ),
+        centerTitle: true,
+      ),
+      body: Form(
+        child: ListView(padding: EdgeInsets.all(40), children: [
+          Text(
+            'Log in',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 20),
+          TextFormField(
+            controller: emailController,
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(
+              labelText: 'Email Address',
+            ),
+          ),
+          TextFormField(
+            controller: passwordController,
+            keyboardType: TextInputType.text,
+            obscureText: true,
+            decoration: InputDecoration(
+              labelText: 'Password',
+            ),
+          ),
+          SizedBox(height: 30),
+          ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.amber,
+                minimumSize: Size(128, 40),
+              ),
+              onPressed: () {
+                print(emailController.text);
+                print(passwordController.text);
+              },
+              child: Text(
+                'Submit',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w400,
+                ),
+              )),
+        ]),
+      ),
+    );
+  }
+}
